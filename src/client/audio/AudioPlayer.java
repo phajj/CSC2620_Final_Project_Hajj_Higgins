4059778ccc18f710f9a9f0bb478e90ade27543d7
jhiggins <jhigginsswe@gmail.com>
2026-05-02 09:59:22 -0400

    Created basic AudioPlayer class to play audio from the device.

--- src/client/audio/AudioPlayer.java
+++ src/client/audio/AudioPlayer.java
@@ -1,13 +1,37 @@
 package client.audio;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.ByteArrayInputStream;
+
 public class AudioPlayer {
+
+    private Clip clip;
 
     public AudioPlayer() {
     }
 
     public void play(byte[] audioData) {
+        stop(); // stop any current playback before starting a new one
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(
+                    new ByteArrayInputStream(audioData));
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+            clip.start();
+        } catch (Exception e) {
+            // Unsupported format or audio system unavailable
+        }
     }
 
     public void stop() {
+        if (clip != null) {
+            if (clip.isRunning()) {
+                clip.stop();
+            }
+            clip.close();
+            clip = null;
+        }
     }
 }
